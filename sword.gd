92bcf0bdd39d38b9002c68c21c350e06a045a1c0
JoshWannaPaas <paasjoshua@yahoo.com>
2024-01-23 23:28:56 -0800

    feat: added spin as 3rd combo

--- sword.gd
+++ sword.gd
@@ -5,10 +5,12 @@ extends Area2D
 func _ready() -> void:
 	var weaponAnim = %AnimationPlayer
 	get_parent().get_parent().test += 1
-	if test % 2 == 0:
+	if test % 3 == 0:
 		weaponAnim.play("swing")
-	else:
+	elif test % 3 == 1:
 		weaponAnim.play("stab")
+	elif test % 3 == 2:
+			weaponAnim.play("spin")
 	await weaponAnim.animation_finished
 	queue_free()
 
